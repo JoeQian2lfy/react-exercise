098e35550d18b40fa5443cc812e40daca5e444f5
JoeQian <953835302@qq.com>
2016-01-13 18:49:59 +0800

    桑心

--- app/main.js
+++ app/main.js
@@ -1,19 +1,11 @@
-/*
-React的语法真他妈鸡巴！
-哔了狗的语法高亮！
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import App from './app.jsx';
 
-//config app root
-const Root = (
-    <Provider store={store}></Provider>
-)
-
-//render
 ReactDOM.render(
-    <Root />,
-    document.querySelector("#root");
+    <Provider store={store}>
+        <App></App>
+    </Provider>,
+    document.querySelector("#root")
 );
